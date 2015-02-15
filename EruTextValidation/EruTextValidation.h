b9fefbc152441c57f42e0d774be44421db9edbba
Göksel Köksal <goksel.koksal@outlook.com>
2015-02-16 00:50:12 +0200

    Initial Commit

---
+++ EruTextValidation/EruTextValidation.h
@@ -0,0 +1,19 @@
+//
+//  EruTextValidation.h
+//  EruTextValidation
+//
+//  Created by Göksel Köksal on 16/02/15.
+//  Copyright (c) 2015 Eru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EruTextValidation.
+FOUNDATION_EXPORT double EruTextValidationVersionNumber;
+
+//! Project version string for EruTextValidation.
+FOUNDATION_EXPORT const unsigned char EruTextValidationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EruTextValidation/PublicHeader.h>
+
+
